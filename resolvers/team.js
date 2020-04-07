fe66a6ecc96ae344682c9866f2e837b5cd8bddb8
SofianSaleh <sofiane.saleh11@egmail.com>
2020-04-07 10:45:10 +0200

    did the auth for the protected routes

--- resolvers/team.js
+++ resolvers/team.js
@@ -1,22 +1,25 @@
 const { formatErrors } = require("../helper");
+const requiresAuth = require("../premissions");
 
 module.exports = {
   Mutation: {
-    createTeam: async (parent, args, { models, user }) => {
-      try {
-        console.log(user);
-        const team = await models.Team.create({ ...args, owner: user.id });
-        console.log(team);
-        return {
-          success: true,
-        };
-      } catch (e) {
-        console.log(e);
-        return {
-          success: false,
-          error: formatErrors(e, models),
-        };
+    createTeam: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          console.log(user);
+          const team = await models.Team.create({ ...args, owner: user.id });
+          console.log(team);
+          return {
+            success: true,
+          };
+        } catch (e) {
+          console.log(e);
+          return {
+            success: false,
+            error: formatErrors(e, models),
+          };
+        }
       }
-    },
+    ),
   },
 };
