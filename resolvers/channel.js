2323519f99a69cf796fe6476a2d8ece2162d2cbb
SofianSaleh <sofiane.saleh11@egmail.com>
2020-04-09 13:24:50 +0200

    finished modifying for today

--- resolvers/channel.js
+++ resolvers/channel.js
@@ -2,16 +2,10 @@ module.exports = {
   Mutation: {
     createChannel: async (parent, args, { models }) => {
       try {
-        return {
-          message: null,
-          success: !!(await models.Channel.create(args))
-        };
+        return !!(await models.Channel.create(args));
       } catch (e) {
-        return {
-          message: e.message,
-          success: false
-        };
+        return false;
       }
-    }
-  }
+    },
+  },
 };
